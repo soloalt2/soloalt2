7c0e09e21b7d8005da848fe7ff679bda3e3f92a1
Solo <cases.businessacc@gmail.com>
2025-02-07 15:39:53 +0400

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const path = require('path');
 
 
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 //middle ware
 app.use(bodyParser.urlencoded({ extended: true }));
